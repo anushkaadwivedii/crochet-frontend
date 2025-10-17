a84d9658b42f04242431fce9183da81325f8a035
anushkaadwivedii <anushkadwivedi71@gmail.com>
2025-10-17 17:21:07 -0500

    Refactor main.jsx to use StrictMode and basename
    Wrap the application in StrictMode and set a basename for BrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 import { useEffect } from 'react';
 
+AOS.init();
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+  <StrictMode>
+    <BrowserRouter basename="/crochet-frontend">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
